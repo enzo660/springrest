5e6d89802e530b7ebea1c7724fc5920bc27e3e66
ptemkar <enzo.660@gmail.com>
2014-09-02 11:59:32 -0700

    changed the package heirarchy

--- src/main/java/com/prateek/GreetingController.java
+++ src/main/java/com/prateek/GreetingController.java
@@ -1,6 +1,7 @@
-package hello;
+package com.prateek;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
